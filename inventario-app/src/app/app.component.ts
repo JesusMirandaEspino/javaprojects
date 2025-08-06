80bb0bd118d3266e9169a6e91ed7ea4fb1c6bf45
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2025-08-06 12:36:54 -0600

    sistema inventarios java y angular

---
+++ inventario-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'inventario-app';
+
+  // run app cd /d E:\PAGINASDISEÑOWEB\java\proyectos\inventario-app
+}
