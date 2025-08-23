c7638c4033269055a556e7521e8ef432556ffb0c
GeraltJM <jesus.voluntadheredada.human@gmail.com>
2025-08-23 07:49:37 -0600

    peliculas app

---
+++ netbeans/Peliculas/src/mx/com/gm/peliculas/dominio/Pelicula.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.gm.peliculas.dominio;
+
+/**
+ *
+ * @author jesus
+ */
+public class Pelicula {
+    private String Nombre;
+    
+    
+    public Pelicula() {
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "Nombre=" + Nombre + '}';
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public Pelicula(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    
+    
+    
+}
---
+++ netbeans/Peliculas/src/mx/com/gm/peliculas/excepciones/LecturaDatosException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.com.gm.peliculas.excepciones;
+
+/**
+ *
+ * @author jesus
+ */
+public class LecturaDatosException extends AccesoDatosEx{
+    
+    public LecturaDatosException(String mensaje) {
+        super(mensaje);
+    }
+    
+}
